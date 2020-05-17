e08d9205ab37dd4a5a60a6ecfe9236b15976b1a6
Subbu <msubbums@gmail.com>
2020-05-17 07:55:11 -0700

    go formatted

--- iterativelinearsearch.go
+++ iterativelinearsearch.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{5,2,8,2,4,3,9,0,6,1}
-	fmt.Println(search(a,5)) // prints true
-	fmt.Println(search(a,25)) //prints false
+	a := []int{5, 2, 8, 2, 4, 3, 9, 0, 6, 1}
+	fmt.Println(search(a, 5))  // prints true
+	fmt.Println(search(a, 25)) //prints false
 }
 
 // finds the key in the array and returns true if found else false.
 func search(a []int, key int) bool {
-	for _,v := range a {
+	for _, v := range a {
 		if v == key {
 			return true
 		}
